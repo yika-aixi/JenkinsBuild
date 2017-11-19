d7b8c6005237687086483e51659c1992a34f8dee
伊卡洛斯 <1375400884@qq.com>
2017-11-19 18:41:56 +0800

    !T 修改测试代码为正常代码

--- JenkinsBuild/Editor/Python/executePy.sh
+++ JenkinsBuild/Editor/Python/executePy.sh
@@ -1,7 +1,7 @@
 echo "调用py的shell被调用~"
 loadxml(){
 	echo "xml路径: $1"
-	config=$(python -c "import LoadConfig; LoadConfig.loadxml('I:\GitHubProject\JenkinsBuild\JenkinsBuild\Editor\Config\AndroidBuildInfo.config')") 
+	config=$(python -c "import LoadConfig; LoadConfig.loadxml('$1')") 
 }
 
 export -f loadxml
--- call.sh
+++ call.sh
@@ -2,7 +2,7 @@
 call(){
 echo "executePy路径: $1"
 source $0 #加载executePy.sh
-configPath="23433242355" #声明配置文件路径,也将是Jenkins使用唯一需要修改的~ **********
+configPath="I:\GitHubProject\JenkinsBuild\JenkinsBuild\Editor\Config\AndroidBuildInfo.config" #声明配置文件路径,也将是Jenkins使用唯一需要修改的~ **********
 cd $1 #进入executePy.sh所在路径,不然在执行py时会报 No module named
 loadxml $configPath #调用 executePy.sh 中的 loadxml 函数并传入配置文件的路径
 echo "解析完成:
