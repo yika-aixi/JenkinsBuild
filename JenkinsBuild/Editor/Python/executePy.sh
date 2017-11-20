2d23de881e9484758fe917a5dc4bb37fab60cd32
Icarus <1375400884@qq.com>
2017-11-20 14:01:58 +0800

    !T 加入“”

--- JenkinsBuild/Editor/Python/executePy.sh
+++ JenkinsBuild/Editor/Python/executePy.sh
@@ -1,6 +1,6 @@
 loadxml(){
 	echo "xml路径: $1"
-	config=$(python -c "import LoadConfig; LoadConfig.loadxml($1)") 
+	config=$(python -c "import LoadConfig; LoadConfig.loadxml("$1")") 
 }
 
 export -f loadxml
