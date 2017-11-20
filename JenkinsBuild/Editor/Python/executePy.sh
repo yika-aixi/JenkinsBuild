71a8648a83d890f05527dace43dd0763e30801ba
Icarus <1375400884@qq.com>
2017-11-20 11:01:12 +0800

    !T 修改进入当前目录的命令

--- JenkinsBuild/Editor/Python/executePy.sh
+++ JenkinsBuild/Editor/Python/executePy.sh
@@ -1,6 +1,6 @@
 echo "调用py的shell被调用~"
-echo "进入当前shell目录~"
-cd $PWD
+echo "进入当前shell目录~ $(cd "$(dirname "$0")";pwd)"
+
 loadxml(){
 	echo "xml路径: $1"
 	config=$(python -c "import LoadConfig; LoadConfig.loadxml('$1')") 
